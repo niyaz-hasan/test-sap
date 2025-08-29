62631ed79bde0070d13cf041457bd664f92fba17
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-08-29 21:09:14 +0530

    added new changes

--- modules/athena/variables.tf
+++ modules/athena/variables.tf
@@ -1,17 +1,11 @@
-variable "database_names" {
-type = list(string)
-description = "List of Athena database names (7 total)."
+variable "athena_workgroup_name" {
+  description = "Athena Workgroup name"
+  type        = string
+  default     = "primary"
 }
 
-
-variable "bucket_mapping" {
-type = map(string)
-description = "Mapping of database_name => bucket_name for query results."
-}
-
-
-variable "sample_query" {
-type = string
-default = "SELECT 1;"
-description = "Sample query for Athena."
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default     = {}
 }
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -47,3 +47,5 @@ resource "aws_s3_bucket_public_access_block" "block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
