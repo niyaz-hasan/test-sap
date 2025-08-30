8dc2c89df2ff3de8a6858a4fa8b034440ece3aec
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-08-31 00:33:14 +0530

    add for_each

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -5,27 +5,28 @@ data "aws_caller_identity" "this" {}
 data "aws_region" "this" {}
 
 locals {
-  account_id   = data.aws_caller_identity.this.account_id
-  region       = var.region == "" ? data.aws_region.this.name : var.region
+  account_id = data.aws_caller_identity.this.account_id
+  region     = var.region == "" ? data.aws_region.this.name : var.region
 }
 
 #####################################  S3 Buckets #################################################
 
 resource "aws_s3_bucket" "data_lake" {
-  count  = length(var.bucket_names)
-  bucket = var.bucket_names[count.index]
+  for_each = toset(var.bucket_names)
+
+  bucket = each.value
   acl    = "private"
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
 
   lifecycle_rule {
-    enabled                          = true
+    enabled = true
     noncurrent_version_expiration {
       days = 30
     }
@@ -39,13 +40,12 @@ resource "aws_s3_bucket" "data_lake" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
-  count  = length(var.bucket_names)
-  bucket = aws_s3_bucket.data_lake[count.index].id
+  for_each = aws_s3_bucket.data_lake
+
+  bucket = each.value.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,7 +1,20 @@
-output "bucket_ids" {
-  value =  aws_s3_bucket.data_lake[*].id
+##################### OUTPUTS #####################
+
+# Output all bucket ARNs as a map (bucket_name => ARN)
+output "bucket_arns" {
+  value = {
+    for k, b in aws_s3_bucket.data_lake : k => b.arn
+  }
 }
 
-output "bucket_arns" {
-  value = aws_s3_bucket.data_lake[*].arn
+# Output all bucket names as a list
+output "bucket_names" {
+  value = [for b in aws_s3_bucket.data_lake : b.bucket]
+}
+
+# Output all bucket IDs as a map (bucket_name => ID)
+output "bucket_ids" {
+  value = {
+    for k, b in aws_s3_bucket.data_lake : k => b.id
+  }
 }
