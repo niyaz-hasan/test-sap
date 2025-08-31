d1b6984b1b91af54128cbde871e60adde01a9634
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-08-31 13:51:50 +0530

    added fix

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,20 +1,9 @@
-############## DATA SOURCE ##################
-
-data "aws_caller_identity" "this" {}
-
-data "aws_region" "this" {}
-
-locals {
-  account_id = data.aws_caller_identity.this.account_id
-  region     = var.region == "" ? data.aws_region.this.name : var.region
-}
-
 #####################################  S3 Buckets #################################################
 
 resource "aws_s3_bucket" "data_lake" {
   for_each = toset(var.bucket_names)
 
-  bucket = each.value-qc
+  bucket = each.value
   acl    = "private"
 
   server_side_encryption_configuration {
