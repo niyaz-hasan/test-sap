6f7b7d1de4c10da09a3fe6b1a24be98c866c4291
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-08-31 12:42:35 +0530

    added

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -1,18 +1,35 @@
-resource "aws_glue_catalog_database" "db" {
-  count = length(var.database_names)
+# Convert database_names and bucket_names into a map for for_each
+locals {
+  db_map = {
+    for idx, db_name in var.database_names :
+    db_name => {
+      name   = db_name
+      bucket = var.bucket_names[idx]
+    }
+  }
+}
 
-  name        = var.database_names[count.index]
-  description = "Glue DB for ${var.database_names[count.index]}"
+
+################################### glue catalog database #########################################################
+
+resource "aws_glue_catalog_database" "db" {
+  for_each = local.db_map
+
+  name        = each.value.name
+  description = "Glue DB for ${each.value.name}"
   parameters  = var.parameters
 }
 
+
+################################# glue crawler #########################################################################
+
 resource "aws_glue_crawler" "crawler" {
-  count        = length(var.database_names)
-  name         = "${var.database_names[count.index]}-crawler"
-  database_name = aws_glue_catalog_database.db[count.index].name
+  for_each     = local.db_map
+  name         = "${each.value.name}-crawler"
+  database_name = aws_glue_catalog_database.db[each.key].name
   role          = var.iam_role_arn
 
   s3_target {
-    path = "s3://${var.bucket_names[count.index]}/"
+    path = "s3://${each.value.bucket}/"
   }
 }
